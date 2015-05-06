d2708d7a737e6a98e06625fa13e7d981fd902f92
Idan Katz <idan@skycure.com>
2015-05-06 21:39:01 +0300

    fix cvss impact associations

--- lib/models/fidius/cve_db/cvss.rb
+++ lib/models/fidius/cve_db/cvss.rb
@@ -1,7 +1,8 @@
 class FIDIUS::CveDb::Cvss < FIDIUS::CveDb::CveDbBase
   attr_accessible :score, :source, :generated_on, :access_vector, :access_complexity, :authentication,
     :confidentiality_impact_id, :integrity_impact_id, :availability_impact_id, :nvd_entry_id
-  has_one :confidentiality_impact
-  has_one :availability_impact
-  has_one :integrity_impact
+
+  belongs_to :confidentiality_impact, class_name: 'FIDIUS::CveDb::Impact', foreign_key: 'confidentiality_impact_id'
+  belongs_to :availability_impact, class_name: 'FIDIUS::CveDb::Impact', foreign_key: 'availability_impact_id'
+  belongs_to :integrity_impact, class_name: 'FIDIUS::CveDb::Impact', foreign_key: 'integrity_impact_id'
 end
